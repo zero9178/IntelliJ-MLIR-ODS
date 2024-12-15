72131c2f4f429dace328bbc3427452f4bf77638c
Markus Böck <markus.boeck02@gmail.com>
2024-12-15 20:43:13 +0100

    [LSP] Simplify descriptor (#21)

--- src/main/kotlin/com/github/zero9178/mlirods/lsp/TableGenLspServerDescriptor.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/lsp/TableGenLspServerDescriptor.kt
@@ -1,19 +1,8 @@
 package com.github.zero9178.mlirods.lsp
 
-import com.github.zero9178.mlirods.service.PluginLifetimeService
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.vfs.AsyncFileListener
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.openapi.vfs.newvfs.events.VFileContentChangeEvent
-import com.intellij.openapi.vfs.newvfs.events.VFileCreateEvent
-import com.intellij.openapi.vfs.newvfs.events.VFileDeleteEvent
-import com.intellij.platform.lsp.api.LspServerListener
 import com.intellij.platform.lsp.api.LspServerManager
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
 import com.intellij.platform.lsp.api.customization.LspCodeActionsSupport
@@ -28,7 +17,7 @@ import com.intellij.platform.lsp.api.customization.LspCodeActionsSupport
  */
 class TableGenLspServerDescriptor(
     executable: VirtualFile, compileCommands: VirtualFile, project: Project
-) : ProjectWideLspServerDescriptor(project, "TableGen"), LspServerListener, Disposable {
+) : ProjectWideLspServerDescriptor(project, "TableGen") {
 
     // Paths in canonical form for comparison and command line creation.
     private val executablePath = executable.path
@@ -37,76 +26,7 @@ class TableGenLspServerDescriptor(
     override fun createCommandLine() = GeneralCommandLine().withExePath(executablePath)
         .withParameters("--tablegen-compilation-database=${compileCommandsPath}")
 
-    init {
-        // Dispose the descriptor and its children when the plugin is unloaded.
-        Disposer.register(project.service<PluginLifetimeService>(), this)
-
-        VirtualFileManager.getInstance().addAsyncFileListener(AsyncFileListener { events ->
-            var lspChanged = false
-            var executableExists = true
-            var compileCommandsExist = true
-
-            // Go over all events and figure out whether the server and compile commands have either changed
-            // or exist again.
-            for (event in events) {
-                val executableAffected = event.path == executablePath
-                val compileCommandsAffected = event.path == compileCommandsPath
-                if (!executableAffected && !compileCommandsAffected) {
-                    continue
-                }
-
-                when (event) {
-                    is VFileCreateEvent -> {
-                        lspChanged = true
-                        if (executableAffected) {
-                            executableExists = true
-                        }
-                        if (compileCommandsAffected) {
-                            compileCommandsExist = true
-                        }
-                        continue
-                    }
-
-                    is VFileDeleteEvent -> {
-                        lspChanged = true
-                        if (executableAffected) {
-                            executableExists = false
-                        }
-                        if (compileCommandsAffected) {
-                            compileCommandsExist = false
-                        }
-                    }
-
-                    is VFileContentChangeEvent -> {
-                        lspChanged = true
-                    }
-                }
-            }
-
-            // No changes to LSP files.
-            if (!lspChanged) return@AsyncFileListener null
-
-            // If one of the two does not exist, do not restart the LSP as it will fail.
-            // The LSP should be available as long as possible.
-            if (!executableExists || !compileCommandsExist) return@AsyncFileListener null
-
-            thisLogger().info("Restarting TableGen LSP due to file changes")
-            return@AsyncFileListener object : AsyncFileListener.ChangeApplier {
-                override fun afterVfsChange() {
-                    restartTableGenLSPAsync(project)
-                }
-            }
-        }, this)
-    }
-
-    override fun dispose() {}
-
     override fun isSupportedFile(file: VirtualFile) = file.extension == "td"
-
-    override fun serverStopped(shutdownNormally: Boolean) {
-        // Dispose the descriptor together with the server shutdown.
-        Disposer.dispose(this)
-    }
 
     /**
      * tblgen-lsp-server at the moment does not support 'codeAction'.
@@ -114,9 +34,6 @@ class TableGenLspServerDescriptor(
      */
     override val lspCodeActionsSupport: LspCodeActionsSupport?
         get() = null
-
-    override val lspServerListener: LspServerListener
-        get() = this
 }
 
 fun restartTableGenLSPAsync(project: Project) {
