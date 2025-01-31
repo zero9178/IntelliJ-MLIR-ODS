54f571467515a42905aaf5740bcbdff19e5dd6c6
Markus Böck <markus.boeck02@gmail.com>
2025-01-31 17:28:13 +0100

    [Highlighting] Implement highlighting fields in `let` (#45)
    This is the first instance of semantic token highlighting by using the
    parser to heuristically highlight identifiers that follow `let`

---
+++ src/main/kotlin/com/github/zero9178/mlirods/language/highlighting/TableGenSemanticTokensAnnotator.kt
@@ -0,0 +1,22 @@
+package com.github.zero9178.mlirods.language.highlighting
+
+import com.github.zero9178.mlirods.language.generated.psi.TableGenLetDirective
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.project.DumbAware
+import com.intellij.psi.PsiElement
+
+private class TableGenSemanticTokensAnnotator : Annotator, DumbAware {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        when (element) {
+            is TableGenLetDirective -> {
+                holder.newSilentAnnotation(HighlightSeverity.TEXT_ATTRIBUTES)
+                    .range(element.identifier)
+                    .textAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD)
+                    .create()
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/com/github/zero9178/mlirods/SemanticTokensAnnotatorTest.kt
@@ -0,0 +1,20 @@
+package com.github.zero9178.mlirods
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class SemanticTokensAnnotatorTest : BasePlatformTestCase() {
+
+    fun `test let identifier`() {
+        myFixture.configureByText(
+            "test.td", """
+            class Foo {
+                string s = ?;
+            }
+            def Foo : Foo {
+                let <text_attr textAttributesKey="DEFAULT_INSTANCE_FIELD">s</text_attr> = "";
+            }
+        """.trimIndent()
+        )
+        myFixture.checkHighlighting(false, true, false)
+    }
+}
