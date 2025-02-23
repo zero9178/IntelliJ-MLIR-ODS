e1d29c3e442d55c2691b7b9bdcfa8a1d65a6cc41
Markus Böck <markus.boeck02@gmail.com>
2025-02-23 22:51:48 +0100

    [TableGen] Traverse scopes to find def identifiers

--- src/main/kotlin/com/github/zero9178/mlirods/language/psi/TableGenIdentifierScopeNode.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/language/psi/TableGenIdentifierScopeNode.kt
@@ -1,9 +1,7 @@
 package com.github.zero9178.mlirods.language.psi
 
-import com.intellij.grazie.utils.parents
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.childLeafs
-import com.jetbrains.rd.util.forEachReversed
+import com.intellij.psi.util.descendants
 
 /**
  * Interface implemented by any [PsiElement] which creates a scope of elements found by 'def' lookup.
@@ -11,13 +9,10 @@ import com.jetbrains.rd.util.forEachReversed
 interface TableGenIdentifierScopeNode : PsiElement {
 
     /**
-     * Returns a sequence of all [TableGenDefNameIdentifierOwner] that is an immediate child of this element in reverse
-     * order.
+     * Returns a sequence of all [TableGenDefNameIdentifierOwner] that are contained within this scope.
      */
     val defs
-        get() = sequence {
-            children.forEachReversed {
-                yield(it)
-            }
-        }.filterIsInstance<TableGenDefNameIdentifierOwner>()
+        get() = descendants {
+            it === this || it !is TableGenIdentifierScopeNode
+        }.drop(1).filterIsInstance<TableGenDefNameIdentifierOwner>()
 }
