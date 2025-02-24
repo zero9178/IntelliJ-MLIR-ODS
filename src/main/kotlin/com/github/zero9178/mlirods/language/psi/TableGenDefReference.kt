7ec0fc4775999120386f5623445fbaf981bb09c3
Markus Böck <markus.boeck02@gmail.com>
2025-02-24 14:20:32 +0100

    [TableGen] Implement correct scope search order relative to identifier

--- src/main/kotlin/com/github/zero9178/mlirods/language/psi/TableGenDefReference.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/language/psi/TableGenDefReference.kt
@@ -2,15 +2,35 @@ package com.github.zero9178.mlirods.language.psi
 
 import com.github.zero9178.mlirods.index.DEF_INDEX
 import com.github.zero9178.mlirods.language.generated.psi.TableGenIdentifierValue
+import com.github.zero9178.mlirods.language.generated.psi.TableGenScopeItem
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.IndexNotReadyException
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.PsiReferenceBase
 import com.intellij.psi.ResolveResult
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
-import com.intellij.psi.util.parents
+import com.intellij.psi.util.descendants
+import com.intellij.psi.util.parentOfType
 import com.intellij.util.concurrency.annotations.RequiresReadLock
+
+/**
+ * Perform a traversal visiting all [PsiElement]s before [root] that are within the same scope, before traversing up to
+ * the next parent scope item.
+ */
+private suspend fun SequenceScope<PsiElement>.upwardsDefSearchTraversal(root: PsiElement) {
+    var before: PsiElement? = root
+    while (before != null) {
+        yieldAll(before.descendants {
+            // Do not traverse into sub-scopes.
+            it !is TableGenIdentifierScopeNode
+        })
+        before = before.prevSibling
+    }
+    val nextParent = root.parentOfType<TableGenScopeItem>(withSelf = false) ?: return
+    upwardsDefSearchTraversal(nextParent)
+}
 
 /**
  * Implements the lookup procedure for 'def's.
@@ -20,11 +40,22 @@ class TableGenDefReference(element: TableGenIdentifierValue) : PsiReferenceBase.
     @RequiresReadLock
     override fun multiResolve(incompleteCode: Boolean): Array<out ResolveResult> {
         val name = element.identifier.text
-        // TODO: This does not take into account the position of [element] in the AST.
-        val def = element.parents(false).filterIsInstance<TableGenIdentifierScopeNode>().firstNotNullOfOrNull {
-            it.defs.find { def ->
-                def.name == name
+
+        val def = sequence {
+            val scopeItem = element.parentOfType<TableGenScopeItem>()
+
+            // Start search at the psi element immediately before this one.
+            val prevSibling = scopeItem?.prevSibling
+            if (prevSibling != null) {
+                upwardsDefSearchTraversal(prevSibling)
+                return@sequence
             }
+
+            // If it is the first element in the scope, traverse up to the parent instead.
+            val parent = scopeItem?.parentOfType<TableGenScopeItem>() ?: return@sequence
+            upwardsDefSearchTraversal(parent)
+        }.filterIsInstance<TableGenDefNameIdentifierOwner>().find {
+            it.name == name
         }
 
         // Lookup in the same file succeeded.
