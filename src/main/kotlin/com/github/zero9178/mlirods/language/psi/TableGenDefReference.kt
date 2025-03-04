685e125bcae595dea0b9b6864ccca305fe7c545f
Markus Böck <markus.boeck02@gmail.com>
2025-03-04 22:19:31 +0100

    [TableGen] Fix `def` search skipping template arguments (#64)

--- src/main/kotlin/com/github/zero9178/mlirods/language/psi/TableGenDefReference.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/language/psi/TableGenDefReference.kt
@@ -1,36 +1,38 @@
 package com.github.zero9178.mlirods.language.psi
 
 import com.github.zero9178.mlirods.index.DEF_INDEX
-import com.github.zero9178.mlirods.language.generated.psi.TableGenIdentifierValue
-import com.github.zero9178.mlirods.language.generated.psi.TableGenScopeItem
+import com.github.zero9178.mlirods.language.generated.psi.*
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.IndexNotReadyException
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.PsiReferenceBase
 import com.intellij.psi.ResolveResult
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
-import com.intellij.psi.util.descendants
-import com.intellij.psi.util.parentOfType
+import com.intellij.psi.util.parents
 import com.intellij.util.concurrency.annotations.RequiresReadLock
 
 /**
- * Perform a traversal visiting all [PsiElement]s before [root] that are within the same scope, before traversing up to
- * the next parent scope item.
+ * Add extra names that are defined by the parent but visible only within its scope.
+ * These must act as if defined at the very beginning of that scope.
  */
-private suspend fun SequenceScope<PsiElement>.upwardsDefSearchTraversal(root: PsiElement) {
-    var before: PsiElement? = root
-    while (before != null) {
-        yieldAll(before.descendants {
-            // Do not traverse into sub-scopes.
-            it !is TableGenIdentifierScopeNode
-        })
-        before = before.prevSibling
+private fun addExtraDefNamesForParent(parent: TableGenScopeItem) = when (parent) {
+    is TableGenAbstractClassStatement -> parent.templateArgDeclList.asReversed().asSequence()
+    else -> emptySequence()
     }
-    val nextParent = root.parentOfType<TableGenScopeItem>(withSelf = false) ?: return
-    upwardsDefSearchTraversal(nextParent)
-}
+
+/**
+ * Returns all [TableGenDefNameIdentifierOwner] by performing a backwards traversal starting from [root] and walking up
+ * parents whenever the start has been reached.
+ */
+private fun traverse(root: TableGenScopeItem): Sequence<TableGenDefNameIdentifierOwner> = sequence {
+    yieldAll(root.itemsBefore(withSelf = true))
+
+    root.parentItem?.let {
+        yieldAll(addExtraDefNamesForParent(it))
+        yieldAll(traverse(it))
+    }
+}.filterIsInstance<TableGenDefNameIdentifierOwner>()
 
 /**
  * Implements the lookup procedure for 'def's.
@@ -41,20 +43,28 @@ class TableGenDefReference(element: TableGenIdentifierValue) : PsiReferenceBase.
     override fun multiResolve(incompleteCode: Boolean): Array<out ResolveResult> {
         val name = element.identifier.text
 
-        val def = sequence {
-            val scopeItem = element.parentOfType<TableGenScopeItem>()
+        val def = run {
+            var hadTemplateArg = false
+            val scopeItem = element.parents(withSelf = false).find {
+                when (it) {
+                    is TableGenTemplateArgDecl -> hadTemplateArg = true
+                }
+                it is TableGenScopeItem
+            } as? TableGenScopeItem ?: return@run emptySequence()
 
-            // Start search at the psi element immediately before this one.
-            val prevSibling = scopeItem?.prevSibling
-            if (prevSibling != null) {
-                upwardsDefSearchTraversal(prevSibling)
-                return@sequence
+            // Implement different sequences depending on where in the Psi we are.
+            var sequence = traverse(scopeItem)
+            when (scopeItem) {
+                is TableGenClassStatement ->
+                    // If coming from the template decl, we do not care to add other template arguments to the search.
+                    // Otherwise, i.e., coming from the parent class list, we must add any template arguments.
+                    if (!hadTemplateArg) sequence = addExtraDefNamesForParent(scopeItem) + sequence
+
+                // Definitions should be skipped.
+                is TableGenDefNameIdentifierOwner -> sequence = sequence.drop(1)
             }
-
-            // If it is the first element in the scope, traverse up to the parent instead.
-            val parent = scopeItem?.parentOfType<TableGenScopeItem>() ?: return@sequence
-            upwardsDefSearchTraversal(parent)
-        }.filterIsInstance<TableGenDefNameIdentifierOwner>().find {
+            sequence
+        }.find {
             it.name == name
         }
 
---
+++ src/main/kotlin/com/github/zero9178/mlirods/language/psi/impl/TableGenScopeItemEx.kt
@@ -0,0 +1,67 @@
+package com.github.zero9178.mlirods.language.psi.impl
+
+import com.github.zero9178.mlirods.language.generated.psi.TableGenDefStatement
+import com.github.zero9178.mlirods.language.generated.psi.TableGenDefvarStatement
+import com.github.zero9178.mlirods.language.generated.psi.TableGenScopeItem
+import com.github.zero9178.mlirods.language.generated.psi.TableGenStatement
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentsOfType
+import com.intellij.psi.util.siblings
+
+/**
+ * Interface used to extend inject methods into [TableGenScopeItem]
+ */
+interface TableGenScopeItemEx : PsiElement {
+
+    private val statementOrSelf: PsiElement
+        get() = when (this) {
+            is TableGenDefStatement, is TableGenDefvarStatement -> {
+                // Should be wrapped in a parent [TableGenStatement].
+                parent!!
+            }
+
+            else -> this
+        }
+
+    private fun itemSiblings(forward: Boolean, withSelf: Boolean) =
+        statementOrSelf.siblings(forward, withSelf).filterIsInstance<TableGenScopeItem>().map {
+            when (it) {
+                is TableGenStatement -> {
+                    it.defStatement?.run {
+                        return@map this
+                    }
+                    it.defvarStatement?.run {
+                        return@map this
+                    }
+                }
+            }
+            return@map it
+        }
+
+    /**
+     * Returns a sequence all items that are defined before this one within the same [com.github.zero9178.mlirods.language.psi.TableGenIdentifierScopeNode].
+     * The sequence is in reverse order starting at either this, or its predecessor, and ending in the first item
+     * syntactically.
+     */
+    fun itemsBefore(withSelf: Boolean = false) = itemSiblings(forward = false, withSelf)
+
+    /**
+     * Returns a sequence of all scope items that are a parent of this starting from the immediate parent.
+     */
+    val parentItems: Sequence<TableGenScopeItem>
+        get() = parentsOfType<TableGenScopeItem>(withSelf = false).filter {
+            if (it !is TableGenStatement) {
+                true
+            } else {
+                // Both of these statements were the previous items.
+                // Filter out this pure container statement in that case.
+                it.defvarStatement == null && it.defStatement == null
+            }
+        }
+
+    /**
+     * Returns the scope item that is immediate parent of this or null if there is none.
+     */
+    val parentItem: TableGenScopeItem?
+        get() = parentItems.firstOrNull()
+}
