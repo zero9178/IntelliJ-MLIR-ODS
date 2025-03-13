bda06ff7d4b12c5b4e1629d21d799dd2b3e58aec
Markus Böck <markus.boeck02@gmail.com>
2025-03-13 15:04:31 +0100

    [TableGen][NFC] Simplify AST structure (#68)

--- src/main/kotlin/com/github/zero9178/mlirods/language/psi/impl/TableGenScopeItemEx.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/language/psi/impl/TableGenScopeItemEx.kt
@@ -1,10 +1,6 @@
 package com.github.zero9178.mlirods.language.psi.impl
 
-import com.github.zero9178.mlirods.language.generated.psi.TableGenClassStatement
-import com.github.zero9178.mlirods.language.generated.psi.TableGenDefStatement
-import com.github.zero9178.mlirods.language.generated.psi.TableGenDefvarStatement
 import com.github.zero9178.mlirods.language.generated.psi.TableGenScopeItem
-import com.github.zero9178.mlirods.language.generated.psi.TableGenStatement
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.parentsOfType
 import com.intellij.psi.util.siblings
@@ -14,34 +10,8 @@ import com.intellij.psi.util.siblings
  */
 interface TableGenScopeItemEx : PsiElement {
 
-    private val statementOrSelf: PsiElement
-        get() = when (this) {
-            is TableGenDefStatement, is TableGenDefvarStatement,
-            is TableGenClassStatement -> {
-                // Should be wrapped in a parent [TableGenStatement].
-                parent!!
-            }
-
-            else -> this
-        }
-
     private fun itemSiblings(forward: Boolean, withSelf: Boolean) =
-        statementOrSelf.siblings(forward, withSelf).filterIsInstance<TableGenScopeItem>().map {
-            when (it) {
-                is TableGenStatement -> {
-                    it.defStatement?.run {
-                        return@map this
-                    }
-                    it.defvarStatement?.run {
-                        return@map this
-                    }
-                    it.classStatement?.run {
-                        return@map this
-                    }
-                }
-            }
-            return@map it
-        }
+        siblings(forward, withSelf).filterIsInstance<TableGenScopeItem>()
 
     /**
      * Returns a sequence all items that are defined before this one within the same [com.github.zero9178.mlirods.language.psi.TableGenIdentifierScopeNode].
@@ -54,15 +24,7 @@ interface TableGenScopeItemEx : PsiElement {
      * Returns a sequence of all scope items that are a parent of this starting from the immediate parent.
      */
     val parentItems: Sequence<TableGenScopeItem>
-        get() = parentsOfType<TableGenScopeItem>(withSelf = false).filter {
-            if (it !is TableGenStatement) {
-                true
-            } else {
-                // Both of these statements were the previous items.
-                // Filter out this pure container statement in that case.
-                it.defvarStatement == null && it.defStatement == null && it.classStatement == null
-            }
-        }
+        get() = parentsOfType<TableGenScopeItem>(withSelf = false)
 
     /**
      * Returns the scope item that is immediate parent of this or null if there is none.
