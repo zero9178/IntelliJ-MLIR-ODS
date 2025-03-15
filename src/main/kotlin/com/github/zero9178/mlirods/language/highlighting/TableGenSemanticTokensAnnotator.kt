d6a07e227534e3ff7856b5d7a5a20530769fbc90
Markus Böck <markus.boeck02@gmail.com>
2025-03-15 15:15:56 +0100

    [TableGen] Highlight field identifier values as fields (#74)

--- src/main/kotlin/com/github/zero9178/mlirods/language/highlighting/TableGenSemanticTokensAnnotator.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/language/highlighting/TableGenSemanticTokensAnnotator.kt
@@ -2,6 +2,8 @@ package com.github.zero9178.mlirods.language.highlighting
 
 import com.github.zero9178.mlirods.color.FIELD
 import com.github.zero9178.mlirods.color.PREPROCESSOR_MACRO_NAME
+import com.github.zero9178.mlirods.language.generated.psi.TableGenFieldBodyItem
+import com.github.zero9178.mlirods.language.generated.psi.TableGenIdentifierValue
 import com.github.zero9178.mlirods.language.generated.psi.TableGenPreprocessorDirective
 import com.github.zero9178.mlirods.language.psi.TableGenFieldIdentifierNode
 import com.intellij.lang.annotation.AnnotationHolder
@@ -10,7 +12,7 @@ import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.project.DumbAware
 import com.intellij.psi.PsiElement
 
-private class TableGenSemanticTokensAnnotator : Annotator, DumbAware {
+private class TableGenDumbAwareSemanticTokensAnnotator : Annotator, DumbAware {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         when (element) {
             is TableGenFieldIdentifierNode -> {
@@ -27,6 +29,22 @@ private class TableGenSemanticTokensAnnotator : Annotator, DumbAware {
                 holder.newSilentAnnotation(HighlightSeverity.TEXT_ATTRIBUTES)
                     .range(identifier)
                     .textAttributes(PREPROCESSOR_MACRO_NAME)
+                    .create()
+            }
+        }
+    }
+}
+
+private class TableGenSemanticTokensAnnotator : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        when (element) {
+            is TableGenIdentifierValue -> {
+                if (element.reference?.resolve() !is TableGenFieldBodyItem)
+                    return
+
+                holder.newSilentAnnotation(HighlightSeverity.TEXT_ATTRIBUTES)
+                    .range(element)
+                    .textAttributes(FIELD)
                     .create()
             }
         }
