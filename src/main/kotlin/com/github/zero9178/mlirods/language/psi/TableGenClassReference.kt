670a41960417e8d840045ad98d0a772e9b6d30d6
Markus Böck <markus.boeck02@gmail.com>
2025-03-16 23:13:55 +0100

    [TableGen][NFC] Implement caching of class references

--- src/main/kotlin/com/github/zero9178/mlirods/language/psi/TableGenClassReference.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/language/psi/TableGenClassReference.kt
@@ -11,6 +11,7 @@ import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.PsiReferenceBase
 import com.intellij.psi.ResolveResult
 import com.intellij.psi.stubs.StubIndex
+import com.intellij.psi.util.CachedValuesManager
 import com.intellij.psi.util.parentsOfType
 import com.intellij.util.concurrency.annotations.RequiresReadLock
 
@@ -20,27 +21,38 @@ import com.intellij.util.concurrency.annotations.RequiresReadLock
 class TableGenClassReference(element: TableGenAbstractClassRef) :
     PsiReferenceBase.Poly<TableGenAbstractClassRef>(element) {
 
-    @RequiresReadLock
-    override fun multiResolve(incompleteCode: Boolean) = disallowTreeLoading {
-        val name = element.className
-        val topLevelElement =
-            element.parentsOfType<TableGenScopeItem>(withSelf = false).lastOrNull()
-                ?: return@disallowTreeLoading emptyArray()
-        val klass = topLevelElement.classStatementsBefore().find {
-            it.name == name
-        }
-
-        // Lookup in the same file succeeded.
-        if (klass != null) return@disallowTreeLoading arrayOf(PsiElementResolveResult(klass))
-
-        val project = element.project
-        if (DumbService.isDumb(project)) throw IndexNotReadyException.create()
-
-        // Otherwise, use the index to search in TableGen files included by this file.
-        CLASS_INDEX.getElements(
-            name,
-            project,
-            TableGenIncludedSearchScope(element, project)
-        ).map { PsiElementResolveResult(it) }.toTypedArray()
+    override fun hashCode(): Int {
+        return element.hashCode()
     }
+
+    override fun equals(other: Any?): Boolean {
+        return element === (other as? TableGenClassReference)?.element
+    }
+
+    @RequiresReadLock
+    override fun multiResolve(incompleteCode: Boolean): Array<out ResolveResult> =
+        CachedValuesManager.getProjectPsiDependentCache(element) {
+            disallowTreeLoading {
+                val name = element.className
+                val topLevelElement =
+                    element.parentsOfType<TableGenScopeItem>(withSelf = false).lastOrNull()
+                        ?: return@disallowTreeLoading emptyArray()
+                val klass = topLevelElement.classStatementsBefore().find {
+                    it.name == name
+                }
+
+                // Lookup in the same file succeeded.
+                if (klass != null) return@disallowTreeLoading arrayOf(PsiElementResolveResult(klass))
+
+                val project = element.project
+                if (DumbService.isDumb(project)) throw IndexNotReadyException.create()
+
+                // Otherwise, use the index to search in TableGen files included by this file.
+                CLASS_INDEX.getElements(
+                    name,
+                    project,
+                    TableGenIncludedSearchScope(element, project)
+                ).map { PsiElementResolveResult(it) }.toTypedArray()
+            }
+        }
 }
