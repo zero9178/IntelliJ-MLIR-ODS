2488e52a5d99123a70aceae9cb50a58734ef844e
Markus Böck <markus.boeck02@gmail.com>
2025-04-12 08:45:49 +0200

    [TableGen] Add `def`s and `class`s to Goto popup

--- src/main/kotlin/com/github/zero9178/mlirods/index/Util.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/index/Util.kt
@@ -5,6 +5,8 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
 import com.intellij.psi.stubs.StubIndexKey
+import com.intellij.util.Processor
+import com.intellij.util.indexing.IdFilter
 
 /**
  * Returns all elements that are mapped to [key] and within files included in [scope].
@@ -21,4 +23,34 @@ inline fun <Key : Any, reified Element : PsiElement> StubIndexKey<Key, Element>.
         scope,
         Element::class.java
     )
+}
+
+/**
+ * Invokes [processor] with all elements mapped to [key] included in [scope].
+ */
+inline fun <Key : Any, reified Element : PsiElement> StubIndexKey<Key, Element>.processElements(
+    key: Key,
+    project: Project,
+    scope: GlobalSearchScope,
+    processor: Processor<in Element>
+) {
+    StubIndex.getInstance().processElements(
+        this,
+        key,
+        project,
+        scope,
+        Element::class.java,
+        processor
+    )
+}
+
+/**
+ * Invokes [processor] with all keys of the index that are included in [scope] and [idFilter].
+ */
+fun <Key : Any, Element : PsiElement> StubIndexKey<Key, Element>.processAllKeys(
+    processor: Processor<in Key>,
+    scope: GlobalSearchScope,
+    idFilter: IdFilter? = null
+) {
+    StubIndex.getInstance().processAllKeys(this, processor, scope, idFilter)
 }
---
+++ src/main/kotlin/com/github/zero9178/mlirods/language/TableGenGotoSymbolContributor.kt
@@ -0,0 +1,29 @@
+package com.github.zero9178.mlirods.language
+
+import com.github.zero9178.mlirods.index.DEF_INDEX
+import com.github.zero9178.mlirods.index.processAllKeys
+import com.github.zero9178.mlirods.index.processElements
+import com.intellij.navigation.ChooseByNameContributorEx
+import com.intellij.navigation.NavigationItem
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.util.Processor
+import com.intellij.util.indexing.FindSymbolParameters
+import com.intellij.util.indexing.IdFilter
+
+private class TableGenGotoSymbolContributor : ChooseByNameContributorEx {
+    override fun processNames(
+        processor: Processor<in String>,
+        scope: GlobalSearchScope,
+        filter: IdFilter?
+    ) {
+        DEF_INDEX.processAllKeys(processor, scope, filter)
+    }
+
+    override fun processElementsWithName(
+        name: String,
+        processor: Processor<in NavigationItem>,
+        parameters: FindSymbolParameters
+    ) {
+        DEF_INDEX.processElements(name, parameters.project, parameters.searchScope, processor)
+    }
+}
