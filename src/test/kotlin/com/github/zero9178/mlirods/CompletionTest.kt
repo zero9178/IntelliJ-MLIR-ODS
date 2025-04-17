da11f7ef4e31648138022377bfde255ad4b7015b
Markus Böck <markus.boeck02@gmail.com>
2025-04-17 22:41:31 +0200

    [Completion] Fix missing completions in dumb mode (#107)
    Field lookup may perform index lookup which throws an exception in dumb
    mode and therefore does not use any so far collected elements.
    
    This PR fixes this by not performing field lookup. Field lookup will be
    readded later with a dedicated field reference contributor

--- src/test/kotlin/com/github/zero9178/mlirods/CompletionTest.kt
+++ src/test/kotlin/com/github/zero9178/mlirods/CompletionTest.kt
@@ -1,5 +1,6 @@
 package com.github.zero9178.mlirods
 
+import com.intellij.testFramework.DumbModeTestUtils
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 class CompletionTest : BasePlatformTestCase() {
@@ -25,5 +26,22 @@ class CompletionTest : BasePlatformTestCase() {
         myFixture.completeBasic()
         val list = requireNotNull(myFixture.lookupElementStrings)
         assertSameElements(list, "values")
+    }
+
+    fun `test dumb field lookup`() {
+        myFixture.configureByText(
+            "test.td", """
+            defvar v = 0;
+            
+            class A : B {
+                int i = <caret>;
+            }
+        """.trimIndent()
+        )
+
+        DumbModeTestUtils.computeInDumbModeSynchronously(project) {
+            myFixture.completeBasic()
+            assertSameElements(requireNotNull(myFixture.lookupElementStrings), "v")
+        }
     }
 }
