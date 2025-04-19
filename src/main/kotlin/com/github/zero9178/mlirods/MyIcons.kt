41f06c738a4532b1c5d841c49fbda7c3013c450d
Markus Böck <markus.boeck02@gmail.com>
2025-04-19 20:08:31 +0200

    [TableGen] Add custom icons for classes and defs (#111)

--- src/main/kotlin/com/github/zero9178/mlirods/MyIcons.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/MyIcons.kt
@@ -5,4 +5,10 @@ import com.intellij.openapi.util.IconLoader
 object MyIcons {
     @JvmField
     val TableGenIcon = IconLoader.getIcon("/icons/tablegen-file.svg", javaClass)
+
+    @JvmField
+    val TableGenClass = IconLoader.getIcon("/icons/tablegen-class.svg", javaClass)
+
+    @JvmField
+    val TableGenDef = IconLoader.getIcon("/icons/tablegen-def.svg", javaClass)
 }
