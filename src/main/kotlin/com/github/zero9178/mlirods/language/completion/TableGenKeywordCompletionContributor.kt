74517fd15e8cc06dac6ffbd1f83fb636dd46b418
Markus Böck <markus.boeck02@gmail.com>
2025-04-25 11:37:24 +0200

    [Completion] Add completion contributor for keywords

---
+++ src/main/kotlin/com/github/zero9178/mlirods/language/completion/TableGenKeywordCompletionContributor.kt
@@ -0,0 +1,187 @@
+package com.github.zero9178.mlirods.language.completion
+
+import com.github.zero9178.mlirods.language.TableGenFile
+import com.github.zero9178.mlirods.language.generated.TableGenTypes.IDENTIFIER
+import com.github.zero9178.mlirods.language.generated.psi.*
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.util.Key
+import com.intellij.patterns.PatternCondition
+import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.patterns.StandardPatterns
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiErrorElement
+import com.intellij.util.ProcessingContext
+
+private val STATEMENT_START = arrayOf(
+    "assert",
+    "class",
+    "def",
+    "defm",
+    "defset",
+    "deftype",
+    "defvar",
+    "dump",
+    "foreach",
+    "if",
+    "include",
+    "let",
+    "multiclass"
+)
+
+private val BODY_ITEM_START = arrayOf(
+    "assert",
+    "let",
+    "defvar",
+)
+private val TYPE_START = arrayOf(
+    "bit",
+    "int",
+    "string",
+    "dag",
+    "code",
+)
+private val TYPE_START_ANGLED = arrayOf(
+    "list",
+    "bits",
+)
+
+private fun keywordLookupElement(text: String) =
+    LookupElementBuilder.create(text).bold()
+
+private val TYPE_NODE_PARENT = Key.create<TableGenClassTypeNode>("TYPE_NODE_PARENT")
+private val TYPE_NODE = psiElement(IDENTIFIER)
+    // An arbitrary identifier within a type node is always parsed as a [TableGenClassTypeNode].
+    .withParent(psiElement(TableGenClassTypeNode::class.java).save(TYPE_NODE_PARENT))
+
+/**
+ * Returns a pattern matching a [TableGenClassTypeNode] within [T].
+ */
+private inline fun <reified T : PsiElement> typeIn() = TYPE_NODE.with(object : PatternCondition<PsiElement>(null) {
+    override fun accepts(
+        t: PsiElement,
+        context: ProcessingContext
+    ): Boolean {
+        return context.get(TYPE_NODE_PARENT)?.parent is T
+    }
+})
+
+private val FIELD_BODY_ITEM_TYPE = typeIn<TableGenFieldBodyItem>()
+private val TEMPLATE_ARG_DECL_TYPE = typeIn<TableGenTemplateArgDecl>()
+private val DEFSET_STATEMENT_TYPE = typeIn<TableGenDefsetStatement>()
+
+private const val INSERT_SPACE_KEY = "NO_SPACE"
+
+private class TableGenKeywordCompletionContributor : CompletionContributor(), DumbAware {
+    init {
+        // Completion for keywords within the top level of a file.
+        extend(
+            CompletionType.BASIC,
+            psiElement(IDENTIFIER)
+                .withParents(PsiErrorElement::class.java, TableGenFile::class.java),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    STATEMENT_START.forEach {
+                        result.addElement(
+                            keywordLookupElement(it).withInsertHandler(AddSpaceInsertHandler.INSTANCE)
+                        )
+                    }
+                }
+            }
+        )
+
+        // Completion for keywords within a class or def.
+        // These get initially parsed as an erroneous field body item.
+        extend(
+            CompletionType.BASIC,
+            FIELD_BODY_ITEM_TYPE,
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    BODY_ITEM_START.forEach {
+                        result.addElement(
+                            keywordLookupElement(it).withInsertHandler(AddSpaceInsertHandler.INSTANCE)
+                        )
+                    }
+                }
+            }
+        )
+
+        // Completion for keywords within a type node.
+        extend(
+            CompletionType.BASIC,
+            StandardPatterns.or(
+                FIELD_BODY_ITEM_TYPE
+                    .save(INSERT_SPACE_KEY),
+                TEMPLATE_ARG_DECL_TYPE
+                    .save(INSERT_SPACE_KEY),
+                DEFSET_STATEMENT_TYPE
+                    .save(INSERT_SPACE_KEY), TYPE_NODE
+            ),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    TYPE_START.forEach {
+                        var element = keywordLookupElement(it)
+                        // Insert spaces afterwards for any constructs where whitespace is used as a delimiter to the
+                        // next token.
+                        if (context.get(INSERT_SPACE_KEY) != null)
+                            element = element.withInsertHandler(AddSpaceInsertHandler.INSTANCE)
+
+                        result.addElement(element)
+                    }
+                }
+            }
+        )
+
+        // Completion for keywords within a type node where angled brackets should be inserted.
+        extend(
+            CompletionType.BASIC,
+            TYPE_NODE,
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    TYPE_START_ANGLED.forEach {
+                        result.addElement(keywordLookupElement(it).withInsertHandler { c, l ->
+                            c.document.insertString(c.tailOffset, "<>")
+                            c.editor.caretModel.moveCaretRelatively(1, 0, false, false, false)
+                        })
+                    }
+                }
+            }
+        )
+
+        // Completion for keywords within a value.
+        extend(
+            CompletionType.BASIC,
+            psiElement(IDENTIFIER).withParent(TableGenIdentifierValue::class.java),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    arrayOf("true", "false").forEach {
+                        result.addElement(keywordLookupElement(it))
+                    }
+                }
+            }
+        )
+
+        // TODO: Suggest "then", "else" and "in".
+    }
+}
--- src/test/kotlin/com/github/zero9178/mlirods/CompletionTest.kt
+++ src/test/kotlin/com/github/zero9178/mlirods/CompletionTest.kt
@@ -2,21 +2,19 @@ package com.github.zero9178.mlirods
 
 import com.github.zero9178.mlirods.model.IncludePaths
 import com.intellij.openapi.application.WriteAction
-import com.intellij.openapi.application.writeAction
-import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.DumbModeTestUtils
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.testFramework.utils.vfs.createDirectory
 import com.intellij.testFramework.utils.vfs.createFile
-import kotlinx.coroutines.runBlocking
+import kotlin.collections.listOf
 
 class CompletionTest : BasePlatformTestCase() {
     fun `test foldl completion`() = doTest(
         """
             defvar values = [1];
             defvar test = !foldl(0, <caret>, acc, i, i);
-        """.trimIndent(), "values"
+        """.trimIndent(), "values", doesNotContain = listOf("acc", "i")
         )
 
 
@@ -24,7 +22,7 @@ class CompletionTest : BasePlatformTestCase() {
         """
             defvar values = [1];
             defvar test = !foreach(i, <caret>, i);
-        """.trimIndent(), "values"
+        """.trimIndent(), "values", doesNotContain = listOf("i")
     )
 
 
@@ -155,7 +153,15 @@ class CompletionTest : BasePlatformTestCase() {
             class A;
             
             class B<<caret>
-        """.trimIndent(), "A"
+        """.trimIndent(),
+            "A",
+            "bit",
+            "int",
+            "string",
+            "dag",
+            "code",
+            "list",
+            "bits",
         )
 
         doDumbTest(
@@ -165,7 +171,18 @@ class CompletionTest : BasePlatformTestCase() {
             class B {
                 <caret>
             }
-        """.trimIndent(), "A"
+        """.trimIndent(),
+            "A",
+            "bit",
+            "int",
+            "string",
+            "dag",
+            "code",
+            "list",
+            "bits",
+            "assert",
+            "let",
+            "defvar",
         )
 
 
@@ -174,23 +191,110 @@ class CompletionTest : BasePlatformTestCase() {
             class A;
             
             class B : <caret>
-        """.trimIndent(), "A"
+        """.trimIndent(), "A",
+            doesNotContain = listOf("int")
         )
     }
 
+    fun `test dumb top level completion`() = doDumbTest(
+        """
+        <caret>
+    """.trimIndent(),
+        "assert",
+        "class",
+        "def",
+        "defm",
+        "defset",
+        "deftype",
+        "defvar",
+        "dump",
+        "foreach",
+        "if",
+        "include",
+        "let",
+        "multiclass"
+    )
 
-    private fun doTest(source: String, vararg expected: String) {
+    fun `test list completion typing`() = doTestTyping(
+        """
+        class A {
+            lis<caret>
+        }
+    """.trimIndent(),
+        """
+        class A {
+            list<<caret>>
+        }
+        """.trimIndent()
+    )
+
+    fun `test bits completion typing`() = doTestTyping(
+        """
+        class A {
+            bits<caret>
+        }
+    """.trimIndent(),
+        """
+        class A {
+            bits<<caret>>
+        }
+        """.trimIndent()
+    )
+
+    fun `test space after field`() = doTestTyping(
+        """
+        class A {
+            int<caret>
+        }
+        """.trimIndent(),
+        """
+        class A {
+            int <caret>
+        }
+        """.trimIndent()
+    )
+
+    fun `test space after template arg`() = doTestTyping(
+        """
+        class A<int<caret>
+        """.trimIndent(),
+        """
+        class A<int <caret>
+        """.trimIndent()
+    )
+
+    fun `test space after defset`() = doTestTyping(
+        """
+        defset int<caret>
+        """.trimIndent(),
+        """
+        defset int <caret>
+        """.trimIndent()
+    )
+
+    private fun doTest(source: String, vararg expected: String, doesNotContain: List<String> = emptyList()) {
         myFixture.configureByText(
             "test.td", source
         )
 
 
         myFixture.completeBasic()
-        assertSameElements(requireNotNull(myFixture.lookupElementStrings), *expected)
+        val collection = requireNotNull(myFixture.lookupElementStrings)
+        assertContainsElements(collection, *expected)
+        assertDoesntContain(collection, doesNotContain)
     }
 
-    private fun doDumbTest(source: String, vararg expected: String) =
+    private fun doTestTyping(source: String, expectedText: String) {
+        myFixture.configureByText(
+            "test.td", source
+        )
+
+        myFixture.completeBasicAllCarets(null)
+        myFixture.checkResult(expectedText)
+    }
+
+    private fun doDumbTest(source: String, vararg expected: String, doesNotContain: List<String> = emptyList()) =
         DumbModeTestUtils.runInDumbModeSynchronously(project) {
-            doTest(source, *expected)
+            doTest(source, *expected, doesNotContain = doesNotContain)
         }
 }
