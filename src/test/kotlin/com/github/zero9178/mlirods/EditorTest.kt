49cd536004cda0b41e226c6373f0b944cbaab230
Markus Böck <markus.boeck02@gmail.com>
2025-04-25 16:35:45 +0200

    [TableGen] Always insert matching braces

---
+++ src/test/kotlin/com/github/zero9178/mlirods/EditorTest.kt
@@ -0,0 +1,24 @@
+package com.github.zero9178.mlirods
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class EditorTest : BasePlatformTestCase() {
+
+    fun `test brace matching`() {
+        for (pair in arrayOf('{' to '}', '[' to ']', '(' to ')')) {
+            val (lhs, rhs) = pair
+            myFixture.configureByText(
+                "test.td", """
+                <caret>
+            """.trimIndent()
+            )
+
+            myFixture.type(lhs)
+            myFixture.checkResult(
+                """
+                ${lhs}<caret>${rhs}
+            """.trimIndent()
+            )
+        }
+    }
+}
