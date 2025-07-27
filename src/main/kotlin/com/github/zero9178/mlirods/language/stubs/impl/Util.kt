39e18a7bb477f0aa4fdcf684c667c8b49a9cf0b4
Markus Böck <markus.boeck02@gmail.com>
2025-07-27 16:38:02 +0200

    [Stubs] Simplify type stubs implementation (#180)

---
+++ src/main/kotlin/com/github/zero9178/mlirods/language/stubs/impl/Util.kt
@@ -0,0 +1,36 @@
+package com.github.zero9178.mlirods.language.stubs.impl
+
+import com.github.zero9178.mlirods.language.stubs.TableGenStubElementType
+import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.IStubElementType
+import com.intellij.psi.stubs.StubBase
+import com.intellij.psi.stubs.StubElement
+import com.intellij.psi.stubs.StubInputStream
+import com.intellij.psi.stubs.StubOutputStream
+
+/**
+ * Abstract base class of stub file types which do not contain any data.
+ */
+abstract class TableGenSingletonStubElementType<StubT : StubElement<*>, PsiT : PsiElement>(
+    debugName: String,
+    psiConstructor: (StubT, TableGenStubElementType<StubT, PsiT>) -> PsiT,
+    private val myStubConstructor: (StubElement<*>?, IStubElementType<*, *>) -> StubT
+) : TableGenStubElementType<StubT, PsiT>(
+    debugName, psiConstructor
+) {
+    final override fun createStub(
+        psi: PsiT, parentStub: StubElement<out PsiElement?>?
+    ): StubT {
+        return myStubConstructor.invoke(parentStub, this)
+    }
+
+    final override fun serialize(stub: StubT, dataStream: StubOutputStream) {}
+
+    final override fun deserialize(
+        dataStream: StubInputStream, parentStub: StubElement<*>?
+    ): StubT {
+        return myStubConstructor.invoke(parentStub, this)
+    }
+
+    override fun isAlwaysLeaf(root: StubBase<*>) = true
+}
