96a826b01d41e9a914c57d92a839a242c2e51d02
Markus Böck <markus.boeck02@gmail.com>
2025-08-03 18:56:31 +0200

    WIP

---
+++ src/main/kotlin/com/github/zero9178/mlirods/language/psi/impl/TableGenStringValueNodeMixin.kt
@@ -0,0 +1,17 @@
+package com.github.zero9178.mlirods.language.psi.impl
+
+import com.github.zero9178.mlirods.language.generated.psi.TableGenStringValueNode
+import com.github.zero9178.mlirods.language.stubs.impl.TableGenStringValueNodeStub
+import com.intellij.extapi.psi.StubBasedPsiElementBase
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.IStubElementType
+
+abstract class TableGenStringValueNodeMixin : StubBasedPsiElementBase<TableGenStringValueNodeStub>,
+    TableGenStringValueNode, PsiElement {
+
+    constructor(node: ASTNode) : super(node)
+
+    constructor(stub: TableGenStringValueNodeStub, stubType: IStubElementType<*, *>) : super(stub, stubType)
+}
+
