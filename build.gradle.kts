4af92f806035edb89fe17499eb3d1f3b71db37f3
Markus Böck <markus.boeck02@gmail.com>
2026-02-05 23:49:39 +0100

    disable searchable options for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
 
 // Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
+    buildSearchableOptions = false
     pluginConfiguration {
         version = providers.gradleProperty("pluginVersion")
 
