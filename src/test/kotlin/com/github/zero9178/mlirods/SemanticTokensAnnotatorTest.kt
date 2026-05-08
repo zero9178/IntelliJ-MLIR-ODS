90633cf82059e95d9d5cd793824fa3fed3341a2f
Markus Böck <markus.boeck02@gmail.com>
2026-05-08 13:01:23 +0200

    [TableGen] Fix field identifier definition for `let` with `prepend/append` (#300)

--- src/test/kotlin/com/github/zero9178/mlirods/SemanticTokensAnnotatorTest.kt
+++ src/test/kotlin/com/github/zero9178/mlirods/SemanticTokensAnnotatorTest.kt
@@ -10,13 +10,13 @@ class SemanticTokensAnnotatorTest : BasePlatformTestCase() {
         
         myFixture.configureByText(
             "test.td", """
-            let <text_attr textAttributesKey="TABLEGEN_FIELD">s</text_attr> = 5 in {
+            let prepend <text_attr textAttributesKey="TABLEGEN_FIELD">s</text_attr> = 5 in {
                 class Foo {
                     string <text_attr textAttributesKey="TABLEGEN_FIELD">s</text_attr> = ?;
                 }
             }
             def Foo : Foo {
-                let <text_attr textAttributesKey="TABLEGEN_FIELD">s</text_attr> = "";
+                let append <text_attr textAttributesKey="TABLEGEN_FIELD">s</text_attr> = "";
             }
             defvar test = Foo.<text_attr textAttributesKey="TABLEGEN_FIELD">s</text_attr>;
             #define <text_attr textAttributesKey="TABLEGEN_PREPROCESSOR_MACRO_NAME">FOO</text_attr>
