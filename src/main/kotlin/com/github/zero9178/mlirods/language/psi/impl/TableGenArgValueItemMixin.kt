99a715f0f92b67a3d773da9165a441ccf3d521d4
Markus Böck <markus.boeck02@gmail.com>
2026-06-18 14:27:22 +0200

    [TableGen] Stub `arg_value_item` (#322)

--- src/main/kotlin/com/github/zero9178/mlirods/language/psi/impl/TableGenArgValueItemMixin.kt
+++ src/main/kotlin/com/github/zero9178/mlirods/language/psi/impl/TableGenArgValueItemMixin.kt
@@ -1,13 +1,17 @@
 package com.github.zero9178.mlirods.language.psi.impl
 
 import com.github.zero9178.mlirods.language.generated.psi.TableGenArgValueItem
-import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.github.zero9178.mlirods.language.stubs.impl.TableGenArgValueItemStub
+import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.lang.ASTNode
+import com.intellij.psi.stubs.IStubElementType
 
-abstract class TableGenArgValueItemMixin : ASTWrapperPsiElement, TableGenArgValueItem {
+abstract class TableGenArgValueItemMixin : StubBasedPsiElementBase<TableGenArgValueItemStub>, TableGenArgValueItem {
 
     constructor(node: ASTNode) : super(node)
 
+    constructor(stub: TableGenArgValueItemStub, stubType: IStubElementType<*, *>) : super(stub, stubType)
+
     override val isNamedArgument: Boolean
-        get() = equalsSign != null
+        get() = greenStub?.isNamedArgument ?: (equalsSign != null)
 }
